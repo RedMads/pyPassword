1baf39338ae2e98a0c83f0eb03be1875eeafc10b
RedMads <67266879+RedMads@users.noreply.github.com>
2021-07-17 08:23:07 -0400

    this file for check different os platforms !

---
+++ src/check_os.py
@@ -0,0 +1,46 @@
+# This File to check if user use linux or termux !
+import os
+
+
+class Check_os:
+
+
+    def __init__(self):
+
+        self.cwd = os.getcwd()
+
+    # check the os by trying to change directory to "/usr"
+    # if no Errors is linux OS
+    # if There's Error is Termux
+    def check_os(self):
+
+        try:
+
+            os.chdir("/usr")
+            os.chdir(self.cwd)
+            return "linux"
+
+        except FileNotFoundError:
+
+            return "termux"
+
+    # check if it is linux !
+    def is_linux(self):
+
+        if self.check_os() == "linux": return True
+        else: return False
+
+    # check if it is termux !
+    def is_termux(self):
+
+        if self.check_os() == "termux": return True
+        else: return False
+
+
+# Test !
+if __name__ == "__main__":
+
+    os_obj = Check_os()
+
+    print("is it linux? : {}".format(os_obj.is_linux()))
+    print("is it termux? : {}".format(os_obj.is_termux()))
