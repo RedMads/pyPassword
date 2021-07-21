d272f48814b80a06aa8cdbc6ced1e81d2eb93f3c
RedMads <67266879+RedMads@users.noreply.github.com>
2021-07-21 13:34:40 -0400

    Delete install_termux.sh

--- install_termux.sh
+++
@@ -1,8 +0,0 @@
-apt update -y && apt upgrade -y
-pkg update -y && pkg upgrade -y
-
-apt install git python python3 python3-pip -y
-pkg install git python python3 python3-pip -y
-
-pip3 install cryptography prettytable bcrypt
-
